8a64d0b2799a046dee72199fa4f3032f3d046980
wenling-lichao <wenling.lichao@gmail.com>
2015-01-08 15:08:33 +0800

    菜单管理 从 ui-route 更换为 ngInculde

--- web/app/privilege/js/privilege.foundation.js
+++ web/app/privilege/js/privilege.foundation.js
@@ -12,35 +12,7 @@ var privilege = angular.module('myApp.privilege', [
     'ngToast'
 
 ])
-privilege.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/userManage'); // 容错处理
-    // 可动态初始化功能节点
-    $stateProvider
-        .state('userManage', {
-            url: '/userManage',
-            views: {
-                'content': {
-                    templateUrl: 'templates/userManage.html'
-                }
-            }
-        })
-        .state('deptManage', {
-            url: '/deptManage',
-            views: {
-                'content': {
-                    templateUrl: 'templates/deptManage.html'
-                }
-            }
-        })
-        .state('roleManage', {
-            url: '/roleManage',
-            views: {
-                'content': {
-                    templateUrl: 'templates/roleManage.html'
-                }
-            }
-        })
-})
+
 privilege.config(['ngToastProvider', function (ngToast) {
     ngToast.configure({
         verticalPosition: 'top',
@@ -51,34 +23,24 @@ privilege.config(['ngToastProvider', function (ngToast) {
     });
 }])
 
-/*
- * 由于整个应用都会和路由打交道，所以这里把$state和$stateParams这两个对象放到$rootScope上，方便其它地方引用和注入。
- */
 privilege.run(['$rootScope', '$state', '$stateParams',
     function ($rootScope, $state, $stateParams) {
-        $rootScope.$state = $state;
-        $rootScope.$stateParams = $stateParams;
         console.clear();
     }])
 
 privilege.controller('PrivilegeCtrl', ['$scope', '$rootScope', '$state', 'requestService',
     function ($scope, $rootScope, $state, requestService) {
-
-    }])
-
-privilege.controller('SidebarCtrl', ['$scope', '$rootScope', '$state', 'requestService',
-    function ($scope, $rootScope, $state, requestService) {
-        requestService.privilegeMenuList().success(function (data, httpStatus) {
-            $scope.menu = data; // 注：content 属性是保留字
-            $state.transitionTo(data[2]['menucode']);
-            $scope.selected = data[2];
+        requestService.privilegeMenuList().success(function (menuList, httpStatus) {
+            $scope.menuList = menuList;
+            $scope.menuTemplateUrl = menuList[2]['templateurl']
+            $scope.selectedMenu = menuList[2];
         })
         $scope.setPage = function (menu) {
-            $state.transitionTo(menu['menucode']);
-            $scope.selected = menu;
+            $scope.menuTemplateUrl = menu['templateurl']
+            $scope.selectedMenu = menu;
         };
-        $scope.isSelected = function (menu) {
-            return $scope.selected === menu ? 'open' : '';
+        $scope.isSelectedMenu = function (menu) {
+            return $scope.selectedMenu === menu ? 'open' : '';
         };
     }])
 
--- web/common/request.js
+++ web/common/request.js
@@ -1,76 +1,77 @@
 angular.module("request", []).factory('requestService', function ($http, $rootScope, util) {
-    var BACKEND_SERVER,
-        url,
-        doGetRequest = function (path) {
-            url = _wrapper(path);
-            return $http.get(url)
-            //return $http.jsonp( url )  // 解决跨域问题
-        },
-        doPostRequest = function (path, data) {
-            url = _wrapper(path);
-            //return $http.post(url, data);
-            return $http({ // 跨域
-                url: url,
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                data: data
-            });
-        },
-        _wrapper = function (path) {
-            if (!util.parseURL(path).query) {
-                path += "?callback=JSON_CALLBACK";
-            } else {
-                path += "&callback=JSON_CALLBACK";
-            }
-            url = BACKEND_SERVER + path;
-            return url;
-        },
-        userDetail = function (userno) {
-            return doGetRequest('userDetail.json?userno=' + userno);
-        },
-        userList = function () {
-            return doGetRequest('userList.json');
-        },
-        appRoleList = function () {
-            return doGetRequest('appRoleList.json');
-        },
-        userRoleList = function (userno) {
-            return doGetRequest('userRoleList.json?userno=' + userno);
-        },
-        saveUser = function (url, data) {
-            return doPostRequest(url, data);
-        },
-        deleteUsers = function (url, data) {
-            return doPostRequest(url, data);
-        },
-        privilegeMenuList = function () {
-            return doGetRequest('privilegeMenu.json');
-        },
-        roleDetail = function(){
-
-        },
-        deptList = function(){
-            return doGetRequest('deptList.json');
-        },
-        menuList = function(){
-            return doGetRequest('voteMenuList.json');
+  var BACKEND_SERVER,
+      url,
+      doGetRequest = function (path) {
+        url = _wrapper(path);
+        return $http.get(url)
+        //return $http.jsonp( url )  // 解决跨域问题
+      },
+      doPostRequest = function (path, data) {
+        url = _wrapper(path);
+        //return $http.post(url, data);
+        return $http({ // 跨域
+          url: url,
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          data: data
+        });
+      },
+      _wrapper = function (path) {
+        if(!util.parseURL(path).query) {
+          path += "?callback=JSON_CALLBACK";
+        } else {
+          path += "&callback=JSON_CALLBACK";
         }
-    //BACKEND_SERVER = "http://192.168.2.117:8081/data/module/privilege/"
-    BACKEND_SERVER = "/json/" // absolute for test
-    return {
-        doGetRequest: doGetRequest,
-        doPostRequest: doPostRequest,
-        userDetail: userDetail,
-        userList: userList,
-        appRoleList: appRoleList,
-        userRoleList: userRoleList,
-        saveUser: saveUser,
-        deleteUsers: deleteUsers,
-        privilegeMenuList: privilegeMenuList,
-        roleDetail: roleDetail,
-        deptList: deptList,
-        menuList: menuList
-    }
+        url = BACKEND_SERVER + path;
+        return url;
+      },
+      userDetail = function (userno) {
+        return doGetRequest('userDetail.json?userno=' + userno);
+      },
+      userList = function () {
+        return doGetRequest('userList.json');
+      },
+      appRoleList = function () {
+        return doGetRequest('appRoleList.json');
+      },
+      userRoleList = function (userno) {
+        return doGetRequest('userRoleList.json?userno=' + userno);
+      },
+      saveUser = function (url, data) {
+        return doPostRequest(url, data);
+      },
+      deleteUsers = function (url, data) {
+        return doPostRequest(url, data);
+      },
+      privilegeMenuList = function () {
+        return doGetRequest('privilegeMenu.json');
+      },
+      roleDetail = function () {
+
+      },
+      deptList = function () {
+        return doGetRequest('deptList.json');
+      },
+      menuList = function () {
+        return doGetRequest('voteMenuList.json');
+      }
+  //BACKEND_SERVER = "http://192.168.2.117:8081/data/module/privilege/"
+  BACKEND_SERVER = "/json/" // absolute for test
+  return {
+    doGetRequest: doGetRequest,
+    doPostRequest: doPostRequest,
+    userDetail: userDetail,
+    userList: userList,
+    appRoleList: appRoleList,
+    userRoleList: userRoleList,
+    saveUser: saveUser,
+    deleteUsers: deleteUsers,
+    privilegeMenuList: privilegeMenuList,
+    roleDetail: roleDetail,
+    deptList: deptList,
+    menuList: menuList
+  }
 })
+
