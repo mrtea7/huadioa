b6da440040b8042cbd4c430d69b664abfda8558e
wenling-lichao <wenling.lichao@gmail.com>
2015-02-04 16:17:19 +0800

    doc 文件

--- web/app/dispatch/js/dispatch.foudation.js
+++ web/app/dispatch/js/dispatch.foudation.js
@@ -33,41 +33,41 @@ dispatch.run(['$rootScope', '$state', '$stateParams',
     //console.clear();
   }])
 
-dispatch.controller('DispatchCtrl', ['$scope', '$rootScope', '$state', 'requestService',
-  function ($scope, $rootScope, $state, requestService) {
-    requestService.faWenMenuList().success(function (menuList, httpStatus) {
-      $scope.menuList = menuList;
-      $scope.menuTemplateUrl = menuList[0]['templateurl']
-      $scope.selectedMenu = menuList[0];
-    })
+dispatch.controller('DispatchCtrl',
+    function ($scope, $rootScope, $state, requestService) {
+      requestService.faWenMenuList().success(function (menuList, httpStatus) {
+        $scope.menuList = menuList;
+        $scope.menuTemplateUrl = menuList[0]['templateurl']
+        $scope.selectedMenu = menuList[0];
+      })
 
-    // for menu and menuChild
-    $scope.setPage = function (menu) {
-      //$scope.menuTemplateUrl = menu['templateurl']
-    };
+      // for menu and menuChild
+      $scope.setPage = function (menu) {
+        //$scope.menuTemplateUrl = menu['templateurl']
+      };
 
-    $scope.toggleSelectMenu = function(menu){
-      if( !$scope.selectedMenu || $scope.selectedMenu !== menu ){
-        $scope.selectedMenu = menu;
-        if(menu.children && menu.children.length && menu.expanded){
+      $scope.toggleSelectMenu = function (menu) {
+        if(!$scope.selectedMenu || $scope.selectedMenu !== menu) {
+          $scope.selectedMenu = menu;
+          if(menu.children && menu.children.length && menu.expanded) {
+            $scope.selectedMenu = ""
+          }
+        } else {
           $scope.selectedMenu = ""
         }
-      }else {
-        $scope.selectedMenu = ""
       }
-    }
 
-    // for menu and menuChild
-    $scope.isSelectedMenu = function (menu) {
-      return $scope.selectedMenu === menu ? 'open' : '';
-    };
+      // for menu and menuChild
+      $scope.isSelectedMenu = function (menu) {
+        return $scope.selectedMenu === menu ? 'open' : '';
+      };
 
-    $scope.hasMenuChildren = function (menu) {
-      if(menu.children && menu.children.length > 0) {
-        //console.log("menu", menu.menuname)
-        return true
+      $scope.hasMenuChildren = function (menu) {
+        if(menu.children && menu.children.length > 0) {
+          //console.log("menu", menu.menuname)
+          return true
+        }
       }
-    }
-  }])
+    })
 
 
